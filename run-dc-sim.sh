dcb5848230f0f2b5552d35aedc70ad434b2df2da
Konstantinos Douloudis <kdoul@unipi.gr>
2021-06-15 21:51:09 +0300

    Run licence plugin.

--- run-dc-sim.sh
+++ run-dc-sim.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 #
-# This work is protected under copyrights held by the members of the
-# TOOP Project Consortium as indicated at
-# http://wiki.ds.unipi.gr/display/TOOP/Contributors
-# (c) 2018-2021. All rights reserved.
+# Copyright 2021 - TOOP Project
 #
-# This work is licensed under the EUPL 1.2.
+# This file and its contents are licensed under the EUPL, Version 1.2
+# or – as soon they will be approved by the European Commission – subsequent
+# versions of the EUPL (the "Licence");
 #
-#  = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-#
-# Licensed under the EUPL, Version 1.2 or – as soon they will be approved
-# by the European Commission - subsequent versions of the EUPL
-# (the "Licence");
 # You may not use this work except in compliance with the Licence.
 # You may obtain a copy of the Licence at:
 #
-#         https://joinup.ec.europa.eu/software/page/eupl
+#       https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#
+# See the Licence for the specific language governing permissions and limitations under the Licence.
 #
 
 
